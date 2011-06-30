d73e92a1119fdab50da2e2c4a14ac6ea835e026e
artyrian <artyrian.rahman@gmail.com>
2011-07-01 01:33:02 +0400

    :(

--- clear.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CLEAR_H_
-#define _CLEAR_H_
-
-#include "main.h"
-
-
-void FreeLng (struct all **);
-
-#endif
--- parse.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PARSE_H_
-#define _PARSE_H_
-
-#include "main.h"
-
-void ReadCommand(struct all *, struct checkers *);
-
-#endif
